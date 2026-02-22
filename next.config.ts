192a448c2002509c7009ec20eaa8beaeba771394
Rickymac123 <ricky@rickymc.co.uk>
2026-02-22 02:46:51 +0000

    Fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
